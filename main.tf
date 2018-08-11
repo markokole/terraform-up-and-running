1597db513889b6575dd872f9bee562a3b14329f7
Marko Kljajic <marko_kole@yahoo.com>
2018-08-11 16:26:02 +0200

    add tags

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    #availability_zone = "us-east-1a"
     ami = "ami-9887c6e7" # centos 7
     instance_type = "t2.micro"
+
+    tags {
+      Name = "terraform-example"
+    }
 }
