f1cbe6e85850b206311e3ee96dd380bdbf4e9402
Marko Kljajic <marko_kole@yahoo.com>
2018-08-11 16:43:10 +0200

    update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-9887c6e7" # centos 7
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance_id}"]
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
     # <<-EOF and EOF are Terraforms heredoc syntax for creating multiline strings
     user_data = <<-EOF
