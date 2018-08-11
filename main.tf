44eaf4603f9473e1d13bce6f94af3ade957ca525
Marko Kljajic <marko_kole@yahoo.com>
2018-08-11 16:22:24 +0200

    update

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "us-east-1"
-  availability_zone = "us-east-1a"
 }
 
 resource "aws_instance" "example" {
+    availability_zone = "us-east-1a"
     ami = "ami-9887c6e7" # centos 7
     instance_type = "t2.micro"
 }
