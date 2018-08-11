100d91600d0d6cd6cd359e55ac4ad5ce6f1ed62e
Marko Kljajic <marko_kole@yahoo.com>
2018-08-11 16:42:13 +0200

    added security group

--- main.tf
+++ main.tf
@@ -5,8 +5,27 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-9887c6e7" # centos 7
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance_id}"]
+
+    # <<-EOF and EOF are Terraforms heredoc syntax for creating multiline strings
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags {
       Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
 }
