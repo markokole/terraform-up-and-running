69ac0ddb96b0d242bdbb02cc05409fab965338ff
Marko Kljajic <marko_kole@yahoo.com>
2018-08-12 13:51:34 +0200

    added s3 main

---
+++ shared-storage-s3/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-aws-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "markokole-terraform-state" # name must be globally unique
+}
+
+versioning {
+  enabled = true
+}
+
+lifecycle {
+  prevent_destroy = true
+}
